717a2286b33b7bf37087ef48f22a0c424ad41c5a
nevets615 <surj316@yahoo.com>
2019-04-12 12:23:38 -0400

    finished my index and reducer working on actions

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -2,6 +2,10 @@
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+import axios from 'axios';
+export const LOADING = 'LOADING';
+export const SUCCESS = 'SUCCESS';
+export const ERROR = 'ERROR';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +17,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+export const getSmurfs = () => {
+  return dispatch => {
+    dispatch({ type : LOADING });
+  axios
+  .get ("http://localhost:3333/smurfs")
+  .then(response => {
+    dispatch({ type: SUCCESS, payload: response.data})
+
+  })
+  .catch(err => dispatch({type: ERROR, payload: 'null'})
+    )
+}
+}
+
+export const addSmurf = smurfData => {
+  return dispatch => {
+    dispatch()
+  }
+}
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { LOADIND,SUCCESS, ERROR } from "../actions/index";
+import { LOADING, SUCCESS, ERROR } from "../actions/index";
 
 
 const initialState =
@@ -9,7 +9,26 @@ const initialState =
    error: null
  }
 
-
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOADING:
+    return Object.assign({}, state, { 
+      fetchingSmurfs: true});
+    case SUCCESS:
+    return Object.assign({}, state, {
+      fetchingSmurfs: false,
+      error: null,
+      smurfs: action.payload
+    } )
+    case ERROR:
+    return Object.assign({}, state, {
+      fetchingSmurfs: false,
+      error: action.payload
+    })
+    default: 
+    return state;
+  }
+}
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
